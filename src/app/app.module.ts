80fb215aedd4e96dc5e3293e651c94e347b01aeb
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-01-21 00:06:50 -0300

    feat: Criado o template do formulário de criar pensamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CriarPensamentoComponent
   ],
   imports: [
     BrowserModule,
