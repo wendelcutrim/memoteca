36eef7b16f3a2a80f4637bf0cb6562235acad48e
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-01-21 12:25:15 -0300

    Criado os binding e metodos do componente do formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { CriarPensamentoComponent } from "./components/pensamentos/criar-pensamento/criar-pensamento.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    CriarPensamentoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        CriarPensamentoComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+interface Pensamento {
+    id: string;
+    conteudo: string;
+    autoria: string;
+    modelo: string;
+}
 
 @Component({
-  selector: 'app-criar-pensamento',
-  templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.scss']
+    selector: "app-criar-pensamento",
+    templateUrl: "./criar-pensamento.component.html",
+    styleUrls: ["./criar-pensamento.component.scss"],
 })
 export class CriarPensamentoComponent {
+    pensamento: Pensamento = {
+        id: "1",
+        conteudo: "Aprendendo Angular",
+        autoria: "Dev",
+        modelo: "",
+    };
 
+    criarPensamento() {
+        alert("Novo pensamento criado!");
+    }
+
+    cancelarPensamento() {
+        alert("Pensamento cancelado!");
+    }
 }
