46f83c97884428602f4b6e28326261cfe2ee0824
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-03-03 20:15:40 -0300

    feat: Editado componente e template de editar-pensamento para ficar com o formulário reativo.

--- src/app/services/pensamento.service.spec.ts
+++ src/app/services/pensamento.service.spec.ts
@@ -1,9 +1,6 @@
-import { Observable } from "rxjs";
 import { TestBed } from "@angular/core/testing";
 
 import { PensamentoService } from "./pensamento.service";
-
-import { Pensamento } from "../interfaces/pensamento";
 
 describe("PensamentoService", () => {
     let service: PensamentoService;
