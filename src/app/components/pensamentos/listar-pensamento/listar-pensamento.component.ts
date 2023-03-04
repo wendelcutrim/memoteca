355660ba22333d2ed15e9ff2559d0058c0cb0461
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-03-04 03:35:12 -0300

    feat: Implementado o sistema de busca, para pesquisar por pensamentos específicos

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -11,24 +11,38 @@ export class ListarPensamentoComponent {
     pensamentos: Array<Pensamento> = [];
     paginaAtual: number = 1;
     haMaisPensamentos: boolean = true;
+    filtro: string = "";
+    limit: number = 6;
 
     constructor(private pensamentoService: PensamentoService) {}
 
     ngOnInit(): void {
         this.pensamentoService
-            .listar(this.paginaAtual)
+            .listar(this.paginaAtual, this.limit, this.filtro)
             .subscribe((pensamentos) => (this.pensamentos = pensamentos));
     }
 
     carregarMaisPensamentos(): void {
         console.log(this.haMaisPensamentos);
         this.pensamentoService
-            .listar(++this.paginaAtual)
+            .listar(++this.paginaAtual, this.limit, this.filtro)
             .subscribe((pensamentos) => {
                 this.pensamentos.push(...pensamentos);
                 if (!pensamentos.length) {
                     this.haMaisPensamentos = false;
                 }
+            });
+    }
+
+    pesquisarPensamentos() {
+        // Reinicilizando a paginação e o botão de carregar mais pensamentos ser renderizado
+        this.paginaAtual = 1;
+        this.haMaisPensamentos = true;
+
+        this.pensamentoService
+            .listar(this.paginaAtual, this.limit, this.filtro)
+            .subscribe((pensamentos) => {
+                this.pensamentos = pensamentos;
             });
     }
 }
